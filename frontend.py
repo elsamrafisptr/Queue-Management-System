2f0aec34f83c7504eb6438ffd6bcbc3ddd706229
elsamrafisptr <elsamrafisptr@gmail.com>
2023-11-26 14:50:45 +0700

    input clear, data belum masuk

--- frontend.py
+++ frontend.py
@@ -1,33 +1,87 @@
 from xmlrpc.client import ServerProxy
-import tkinter as tk
-from tkinter import messagebox
 import json
-import time
+import datetime
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showerror
 
-class MedicalQueueApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Medical Queue System")
 
-        self.label = tk.Label(root, text="Antrean Registrasi Medis", font=("Helvetica", 16))
-        self.label.pack(pady=10)
+class TemperatureConverter:
+    @staticmethod
+    def fahrenheit_to_celsius(f):
+        return (f - 32) * 5 / 9
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Telkom Hospital Queue Management System')
+        self.geometry('600x600')
+        self.resizable(False, False)
         
-        self.antrean_info_label = tk.Label(root, text="")
-        self.antrean_info_label.pack(pady=10)
+        # field options
+        options = {'padx': 5, 'pady': 5}
 
-        self.name_label = tk.Label(root, text='Username')
-        self.name_entry = tk.Entry(root, textvariable = name_var)
+        # Nomor Rekam Medis label
+        self.no_medis_label = ttk.Label(self, text='No. Rekam Medis')
+        self.no_medis_label.grid(column=0, row=0, **options)
+
+        # Nomor Rekam Medis entry
+        self.no_medis = tk.StringVar()
+        self.no_medis_entry = ttk.Entry(self, textvariable=self.no_medis)
+        self.no_medis_entry.grid(column=1, row=0, columnspan=2, **options)
+        self.no_medis_entry.focus()
         
-        self.pass_label = tk.Label(root, text='Password')
-        self.pass_entry = tk.Entry(root, textvariable = pass_var)
+        # Nama Lengkap label
+        self.nama_label = ttk.Label(self, text='Nama Lengkap')
+        self.nama_label.grid(column=0, row=1, **options)
 
-        self.update_button = tk.Button(root, text="Daftar Antrean", command=self.update_antrean)
-        self.update_button.pack(pady=10)
+        # Nama Lengkap entry
+        self.nama = tk.StringVar()
+        self.nama_entry = ttk.Entry(self, textvariable=self.nama)
+        self.nama_entry.grid(column=1, row=1, columnspan=2, **options)
+        self.nama_entry.focus()
+        
+        # Tanggal Lahir label
+        self.ttl_label = ttk.Label(self, text='Tempat & Tanggal Lahir')
+        self.ttl_label.grid(column=0, row=2, **options)
 
+        # Tanggal Lahir entry
+        self.ttl = tk.StringVar()
+        self.ttl_entry = ttk.Entry(self, textvariable=self.ttl)
+        self.ttl_entry.grid(column=1, row=2, columnspan=2, **options)
+        self.ttl_entry.focus()
+
+        self.convert_button = ttk.Button(self, text='Daftar Antrean')
+        self.convert_button['command'] = self.update_antrean
+        self.convert_button.grid(row=3, columnspan=3, **options)
+
+        # result label
+        self.result_label = ttk.Label(self)
+        self.result_label.grid(row=4, columnspan=2, **options)
+
+        # add padding to the frame and show it
+        # self.grid(padx=10, pady=10)
+        
         self.server_proxy = ServerProxy('http://127.0.0.1:8000')
-        #self.update_antrean()
-        #self.root.after(5000, self.auto_update_antrean)  # Auto-update every 5 seconds
-
+        
+    def convert(self):
+        """  Handle button click event
+        """
+        try:
+            name = name_var.get()
+            password = pass_var.get()
+            i = 1
+            print("The name is : " + name)
+            print("The password is : " + password)
+            info_text = f"Nomor Antrean: {antrean_data['{i}']}\nEstimasi Waktu: {antrean_data['estimasi_waktu']}"
+            self.antrean_info_label.config(text=info_text)
+            name_var.set("")
+            pass_var.set("")
+            i = i+1
+            
+        except ValueError as error:
+            showerror(title='Error', message=error)
+    
     def update_antrean(self):
         try:
             antrean_data_str = self.server_proxy.get_antrean()
@@ -35,33 +89,19 @@ class MedicalQueueApp:
             antrean_data = json.loads(antrean_data_str)
 
             if antrean_data:
-                name = name_var.get()
-                password = pass_var.get()
                 i = 1
-                print("The name is : " + name)
-                print("The password is : " + password)
-                info_text = f"Nomor Antrean: {antrean_data['{i}']}\nEstimasi Waktu: {antrean_data['estimasi_waktu']}"
-                self.antrean_info_label.config(text=info_text)
-                name_var.set("")
-                pass_var.set("")
-                i = i+1
+                no_rekam_medis = self.no_medis.get()
+                nama_lengkap = self.nama.get()
+                result = f'Pasien Nomor Antrean ke-{i} Nama = {nama_lengkap}, No. Rekam Medis = {no_rekam_medis}'
+                self.result_label.config(text=result)
+                i = i + 1                
             else:
-                self.antrean_info_label.config(text="Tidak ada data antrean.")
+                self.result_label.config(text="Tidak ada data antrean.")
 
         except Exception as e:
             print(f"Error updating antrean: {e}")
-            messagebox.showerror("Error", "Gagal mengupdate antrean. Periksa koneksi Anda.") 
-    
-    def auto_update_antrean(self):
-        self.update_antrean()
-        self.root.after(5000, self.auto_update_antrean)
+            showerror("Error", "Gagal mengupdate antrean. Periksa koneksi Anda.") 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    name_var = tk.StringVar()
-    pass_var = tk.StringVar()
-    
-    app = MedicalQueueApp(root)
-    
-    root.geometry("300x200")
-    root.mainloop()
+    app = App()
+    app.mainloop()
